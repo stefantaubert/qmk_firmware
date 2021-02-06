27acab478efb863280699cbb19f4023c850b18b8
Stefan Taubert <stefan.taubert@posteo.de>
2021-02-06 13:20:37 +0100

    added image of satan layout

--- keyboards/gh60/satan/keymaps/stefan/keymap.c
+++ keyboards/gh60/satan/keymaps/stefan/keymap.c
@@ -69,6 +69,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   	return true;
 };
 
+// layout: http://www.keyboard-layout-editor.com/#/gists/8812df481493a27dee1392b693bc2ec3
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASIS] = LAYOUT_60_iso(
 		KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_BSPACE,
